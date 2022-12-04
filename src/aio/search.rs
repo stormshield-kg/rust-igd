b7854311829bf450d49b3a2c75dba6013efb0790
Darius <darius.clark93@gmail.com>
2022-12-04 18:58:37 -0500

    aio/search_gateway: Remove check

--- src/aio/search.rs
+++ src/aio/search.rs
@@ -32,14 +32,6 @@ pub async fn search_gateway(options: SearchOptions) -> Result<Gateway, SearchErr
     let (control_schema_url, control_url) = get_control_urls(&addr, &root_url).await?;
     let control_schema = get_control_schemas(&addr, &control_schema_url).await?;
 
-    let addr = match addr {
-        SocketAddr::V4(a) => Ok(a),
-        _ => {
-            warn!("unsupported IPv6 gateway response from addr: {}", addr);
-            Err(SearchError::InvalidResponse)
-        }
-    }?;
-
     Ok(Gateway {
         addr,
         root_url,

894b30bf991a980f2c1fa92255117d2cfc203a7f
Darius <darius.clark@proton.me>
2023-06-14 00:43:27 -0400

    fix: Dont panic while searching gateway (ported from c01446a4dd93d8ed1a7a1bd16b68fcd01e69122d)

--- src/search.rs
+++ src/search.rs
@@ -2,6 +2,9 @@ use std::collections::HashMap;
 use std::net::{SocketAddr, UdpSocket};
 use std::str;
 
+use attohttpc::{Method, RequestBuilder};
+use log::debug;
+
 use crate::common::{messages, parsing, SearchOptions};
 use crate::errors::SearchError;
 use crate::gateway::Gateway;
@@ -37,12 +40,24 @@ pub fn search_gateway(options: SearchOptions) -> Result<Gateway, SearchError> {
 
         let (control_schema_url, control_url) = match get_control_urls(&addr, &root_url) {
             Ok(o) => o,
-            Err(..) => continue,
+            Err(e) => {
+                debug!(
+                    "Error has occurred while getting control urls. error: {}, addr: {}, root_url: {}",
+                    e, addr, root_url
+                );
+                continue;
+            }
         };
 
         let control_schema = match get_schemas(&addr, &control_schema_url) {
             Ok(o) => o,
-            Err(..) => continue,
+            Err(e) => {
+                debug!(
+                    "Error has occurred while getting schemas. error: {}, addr: {}, control_schema_url: {}",
+                    e, addr, control_schema_url
+                );
+                continue;
+            }
         };
 
         return Ok(Gateway {
@@ -57,12 +72,22 @@ pub fn search_gateway(options: SearchOptions) -> Result<Gateway, SearchError> {
 
 fn get_control_urls(addr: &SocketAddr, root_url: &str) -> Result<(String, String), SearchError> {
     let url = format!("http://{}:{}{}", addr.ip(), addr.port(), root_url);
-    let response = attohttpc::get(url).send()?;
-    parsing::parse_control_urls(&response.bytes()?[..])
+    match RequestBuilder::try_new(Method::GET, url) {
+        Ok(request_builder) => {
+            let response = request_builder.send()?;
+            parsing::parse_control_urls(&response.bytes()?[..])
+        }
+        Err(error) => Err(SearchError::HttpError(error)),
+    }
 }
 
 fn get_schemas(addr: &SocketAddr, control_schema_url: &str) -> Result<HashMap<String, Vec<String>>, SearchError> {
     let url = format!("http://{}:{}{}", addr.ip(), addr.port(), control_schema_url);
-    let response = attohttpc::get(url).send()?;
-    parsing::parse_schemas(&response.bytes()?[..])
+    match RequestBuilder::try_new(Method::GET, url) {
+        Ok(request_builder) => {
+            let response = request_builder.send()?;
+            parsing::parse_schemas(&response.bytes()?[..])
+        }
+        Err(error) => Err(SearchError::HttpError(error)),
+    }
 }
